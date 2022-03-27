a3c47ec0e8aad1a39d9b6518e97ebe4ce9a767d7
Jordi Ferré <jferrecontreras@gmail.com>
2022-03-27 18:40:33 +0200

    Created Footer component.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,13 @@
 function Footer() {
   return (
 	  <footer>
-	      <p>Default Footer</p>
+	      <div className="circle">
+			  <span>60"</span><br /><span>SixtySeconds</span><br /><hr />
+			  <a href="https://www.linkedin.com" target="_blank">{ /* TODO: add linkedIn icon */ }</a>
+			  <a href="https://twitter.com" target="_blank">{ /* TODO: add twitter icon */ }</a>
+			  <a href="https://www.instagram.com" target="_blank">{ /* TODO: add instagram icon */ }</a>
+			  <a href="https://www.facebook.com" target="_blank">{ /* TODO: add facebook icon */ }</a>
+		  </div>
 	  </footer>
   );
 }
