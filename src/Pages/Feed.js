59266a12b25d828d7a24b45902721693a332277a
mariaacosta <mariaacosta2630@gmail.com>
2022-04-13 22:19:25 +0200

    Eliminado el doble navbar del Feed

--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 //Components
-import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import Categories from '../Components/Categories'
 import Posts from '../Components/Posts'
@@ -10,7 +9,6 @@ export default class Feed extends Component {
   render() {
     return (
       <div>
-        <Navbar/>
         <Categories/>
         <Posts />
         <Footer/>
